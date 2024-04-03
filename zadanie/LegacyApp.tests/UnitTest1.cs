3cab4ec434def2e4607eff03ce1ec8f32769a438
fz3002 <franc3002@gmail.com>
2024-04-03 17:51:53 +0200

    Fix unit testing

--- zadanie/LegacyApp.tests/UnitTest1.cs
+++ zadanie/LegacyApp.tests/UnitTest1.cs
@@ -2,37 +2,59 @@ namespace LegacyApp.tests;
 
 public class UnitTest1
 {
+    UserService us = new UserService();
+
     [Fact]
-    public void Test1()
+    public void AddUser_WhenUserIsNormalClientAndCreditUnder500_ShouldNotAddUserAndReturnFalse()
     {
-        UserService us = new UserService();
         us.AddUser("Jan", "Kowalski", "kowalski@wp.pl", new DateTime(1982, 2,2), 1);
-        Client client = new Client();
-        var name = client.Name;
-        var id = client.ClientId;
-        var email = client.Email;
-        var address = client.Address;
-        ClientRepository cr = new ClientRepository();
-        User user = new User();
     }
+
     [Fact]
-    public void Test2()
+    public void AddUser_WhenBirthDateBiggerThenToday_ShouldReturnFalseAndNotAddUser()
     {
-        UserService us = new UserService();
         us.AddUser("Jan", "Kowalski", "kowalski@wp.pl", new DateTime(2048, 2,2), 1);
+    }
+
+    [Fact]
+    public void AddUser_WhenEmailWronglyFormated_ShouldReturnFalseAndNotAddUser()
+    {
         us.AddUser("Jan", "Kowalski", "kowalskiwppl", new DateTime(1982, 2,2), 1);
+    }
+
+    [Fact]
+    public void AddUser_WhenNameNotGiven_ShouldReturnFalseAndNotAddUser()
+    {
         us.AddUser(null, "Kowalski", "kowalski@wp.pl", new DateTime(1982, 2,2), 1);
+    }
+
+    [Fact]
+    public void AddUser_WhenSurnameNotGiven_ShouldReturnFalseAndNotAddUser()
+    {
         us.AddUser("Jan", null, "kowalski@wp.pl", new DateTime(1982, 2,2), 1);
-        us.AddUser("Jan", "Kowalski", "kowalskiwp.pl", new DateTime(1982, 2,2), 1);
+    }
+
+    [Fact]
+    public void AddUser_WhenAgeUnder21_ShouldReturnFalseAndNotAddUser()
+    {
         us.AddUser("Jan", "Kowalski", "kowalski@wp.pl", new DateTime(2010, 4,2), 1);
-        us.AddUser("Jan", "Kowalski", "kowalski@wp.pl", new DateTime(2010, 3,30), 1);
+    }
+
+    [Fact]
+    public void AddUser_WhenUserDidntHaveBirhtdayYet_ShouldDecreaseAgeByOneReturnFalseAndNotAddUser()
+    {
+        us.AddUser("Jan", "Kowalski", "kowalski@wp.pl", new DateTime(2000, 3,30), 1);
     }
         
     [Fact]
-    public void Test3()
+    public void AddUser_WhenUserIsVeryImportantClient_ShouldAddUserEvenWithCreditLimitUnder500()
     {
-        UserService us = new UserService();
         us.AddUser("Mariusz", "Malewski", "malewski@gmail.pl", new DateTime(1974, 2,2), 2);
+    }
+
+    [Fact]
+    public void AddUser_WhenUserIsImportantClient_ShouldDoubleCreditCardLimit()
+    {
         us.AddUser("John", "Smith", "smith@gmail.pl", new DateTime(1974, 2,2), 3);
     }
 }
